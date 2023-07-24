b1e616d6e91276c015767476c9da1a427469f600
doobom <rovecat@gmail.com>
2023-07-24 14:46:58 +0800

    add duolingo.com.js

---
+++ duolingo.com.js
@@ -0,0 +1,19 @@
+/**************************************
+
+[rewrite_local]
+^https?:\/\/ios-api-2\.duolingo\.com\/2017-06-30\/batch url script-response-body https://raw.githubusercontent.com/5oops/laughing-octo-pancake/master/duolingo.com.js
+
+[mitm]
+hostname = ios-api-2.duolingo.com
+
+*************************************/
+
+const url = $request.url
+const isCheckUrl = (url) => url.includes('ios-api-2.duolingo.com/2017-06-30/batch')
+
+if (isCheckUrl(url) && $response.status == 200) {
+  var rBody = $response.body;
+  rBody = rBody.replace(/isFreeTrialPeriod\\":\s*\w+/, 'isFreeTrialPeriod\\":false').replace(/plusStatus\\":\s*\\"\w+/, 'plusStatus\\":\\"PLUS');
+
+  $done({ rBody })
+}
