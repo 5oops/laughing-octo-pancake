9616aaf57d22ab93f31078109e1d06ff8b793fa0
doobom <rovecat@gmail.com>
2023-07-24 15:41:40 +0800

    fix

--- duolingo.com.js
+++ duolingo.com.js
@@ -14,9 +14,10 @@ const isCheckUrl = (url) => url.includes('ios-api-2.duolingo.com/2017-06-30/batc
 if (isCheckUrl(url)) {
   var rBody = $response.body;
   rBody = rBody.replace(/has_item_gold_subscription\\":\s*\w+/g, 'has_item_gold_subscription\\":true')
-                .replace(/premium_free_trial_period\\":\s*\w+/g, 'premium_free_trial_period\\":true')
+                .replace(/premium_free_trial_period\\":\s*\w+/g, 'premium_free_trial_period\\":false')
                 .replace(/has_item_premium_subscription\\":\s*\w+/g, 'has_item_premium_subscription\\":true')
                 .replace(/premium_expected_expiration\\":\s*(\d+)/g, 'premium_expected_expiration\\":1708739200000')
+                .replace(/expectedExpiration\\":\s*(\d+)/g, 'expectedExpiration\\":1708739200000')
                 .replace(/isFreeTrialPeriod\\":\s*\w+/g, 'isFreeTrialPeriod\\":false')
                 .replace(/plusStatus\\":\s*\\"\w+/g, 'plusStatus\\":\\"PLUS');
 
