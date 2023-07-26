4a7bb77c1eebfb5140472351f566a0e6506ae749
doobom <rovecat@gmail.com>
2023-07-26 17:09:52 +0800

    add emby.js

---
+++ Emby.js
@@ -0,0 +1,30 @@
+/***
+ * from: http://github.com/Tartarus2014/Script.git
+ * 
+[rewrite_local]
+# EmbyPremiere
+^https?:\/\/mb3admin\.com\/admin\/service\/registration\/validateDevice.+ url script-response-body https://raw.githubusercontent.com/5oops/laughing-octo-pancake/master/Emby.js
+
+hostname = mb3admin.com
+**/
+
+const CHECK_URL = 'mb3admin.com/admin/service/registration/validateDevice'
+
+const url = $request.url
+const isCheckUrl = (url) => url.includes(CHECK_URL)
+
+if (isCheckUrl(url) && $response.status != 200) {
+  const unlock = {
+    cacheExpirationDays: 999,
+    resultCode: 'GOOD',
+    message: 'Device Valid'
+  }
+
+  //const status = 200
+  //const headers = $response.headers
+  //const body = JSON.stringify(unlock)
+
+  $done({ status: 200, headers: $response.headers, body: JSON.stringify(unlock) })
+} else {
+  $done({})
+}
