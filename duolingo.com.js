2614af63d1ae804cca9df2fe1a7a480122c9280e
doobom <rovecat@gmail.com>
2024-10-01 00:23:33 +0800

    Update duolingo.com.js

--- duolingo.com.js
+++ duolingo.com.js
@@ -32,7 +32,7 @@ if (isCheckUrl(url)) {
 }
 if ((url.indexOf('ios-api-2.duolingo.com/2017-06-30/users/') !== -1 || url.indexOf('ios-api-2.duolingo.com/2023-05-23/users/') !== -1 ) && url.indexOf('available-features') !== -1 ) {
     const unlock = {
-        "purchasableFeatures": ["CAN_PURCHASE_IAP", "CAN_PURCHASE_SUBSCRIPTION"],
+        "purchasableFeatures": ["CAN_PURCHASE_IAP", "CAN_PURCHASE_SUBSCRIPTION", "CAN_PURCHASE_MAX"],
         "subscriptionFeatures": ["NO_NETWORK_ADS", "UNLIMITED_HEARTS", "LEGENDARY_LEVEL", "MISTAKES_INBOX", "MASTERY_QUIZ", "NO_SUPER_PROMOS", "LICENSED_SONGS", "CHAT_TUTORS", "FACETIME", "VIDEO_CALL_IN_PATH", "VIDEO_CALL_IN_PRACTICE_HUB","CAN_ADD_SECONDARY_MEMBERS"]
     };
     $done( { 'body': JSON.stringify(unlock) } );
